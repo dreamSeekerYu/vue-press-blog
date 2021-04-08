a844cfe9c5756ed42a4402bd555dec48d051c8ee
dreamSeekerYu <yupeng.bai@ucarinc.com>
2021-04-08 10:31:14 +0800

    feat✨ 添加 reco 主题

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  // theme: 'vdoing', // 使用依赖包主题
+  // theme: 'reco', // 使用依赖包主题
+
+  title: "白小哥",
+  description: '专注web前端学习与总结。JavaScript,ES6,vue,css3,html5',
+  markdown: {
+    lineNumbers: true, // 代码行号
+  },
+
+
+}
